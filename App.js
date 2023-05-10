2426539e73983d2a2c05b338d313993adad09d70
Baochuan Lu <lubaochuan@gmail.com>
2023-05-10 10:08:10 -0500

    show a webpage with webview

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { WebView } from 'react-native-webview';
+
+const MyWebComponent = () => {
+  return <WebView source={{ uri: 'https://www.google.com/' }} style={{ flex: 1 }} />;
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+    <View style={{ flex: 1 }}>
+      <MyWebComponent />
       <StatusBar style="auto" />
     </View>
+
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
---
+++ README.md
@@ -0,0 +1,5 @@
+This example app render a webview using https://github.com/react-native-webview/react-native-webview
+
+Run `npx create-expo-app webview` to create a "hello world" app.
+
+Run `expo install react-native-webview` to install and autolink dependency. (https://blog.logrocket.com/react-native-webview-a-complete-guide/)
